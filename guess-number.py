578dc2087d91ed85b261a7baf2e1783f00d56113
Seclusion <qianqian1903@live.cn>
2019-07-10 01:58:30 +0800

    basic guess number

---
+++ guess-number.py
@@ -0,0 +1,14 @@
+# 让user重复猜数字游戏
+# 猜对的话印出猜对了
+#　错的话告诉他比答案大 / 小
+import random
+r = random.randint(1, 100)
+while True:
+    user_input = int(input("请输入数字: "))
+    if user_input == r:
+        print("恭喜，你猜对了")
+        break
+    elif user_input > r:
+        print("您输入的数字比答案大")
+    elif user_input < r:
+        print("您输入的数字比答案小")
