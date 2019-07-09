e4c9537b847d1cbb6b292913ed9c55dba9551421
Seclusion <qianqian1903@live.cn>
2019-07-10 02:28:17 +0800

    added user enter start and end value

--- guess-number.py
+++ guess-number.py
@@ -1,8 +1,12 @@
 # 让user重复猜数字游戏
 # 猜对的话印出猜对了
-#　错的话告诉他比答案大 / 小
+# 错的话告诉他比答案大 / 小
+# 让user 输入开始的value 和结束的value
+
 import random
-r = random.randint(1, 100)
+start = int(input("请输入开始值: "))
+end = int(input("请输入结束值: "))
+r = random.randint(start, end)
 count = 0
 while True:
     user_input = int(input("请输入数字: "))
